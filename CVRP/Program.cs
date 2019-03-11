b1877109070193d2487607a422729d5cd937f144
Maciek Gorczyca <maciek.gorczyca@poczta.fm>
2019-03-11 16:58:06 +0100

    Założenie solucji

---
+++ CVRP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CVRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
